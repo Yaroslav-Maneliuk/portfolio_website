ab91014bf4141d82a81285008c7999f0b678034f
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-11-13 19:14:34 +0200

    Add Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assets/me.png";
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Yaroslav Maneliuk</h1>
+        <h5 className="text-light">Fullstack JS Developer</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
 
-export default Header
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+
+const HeaderSocial = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        <FaGithub />
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <BsInstagram />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocial;
